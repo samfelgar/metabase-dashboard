3c1dff6fe0921c09076fe6ed043ce886f2e2b29f
Samuel Felipe <samfelgar@gmail.com>
2021-12-21 05:16:59 -0300

    Remove the extra api call and refactor the component

--- resources/js/components/Tool.vue
+++ resources/js/components/Tool.vue
@@ -1,44 +1,32 @@
 <template>
-    <div>
-        <heading class="mb-6">{{ title }}</heading>
-        <iframe
-            :src="iframeUrl"
-            frameborder="0"
-            style="width: 100%; min-height: 100vh;"
-            allowtransparency
-        ></iframe>
-    </div>
+  <div>
+    <heading class="mb-6">{{ title }}</heading>
+    <iframe
+        :src="url"
+        allowtransparency
+    ></iframe>
+  </div>
 </template>
 
 <script>
 export default {
-    data: function () {
-        return {
-            iframeUrl: '',
-        };
+  props: ['identifier'],
+
+  computed: {
+    url() {
+      return Nova.config[this.identifier].url;
     },
 
-    beforeMount() {
-        this.fetchIframeUrl();
+    title() {
+      return Nova.config[this.identifier].title;
     },
-
-    computed: {
-        title: function () {
-            return Nova.config.kpiDashboard.title;
-        }
-    },
-
-    methods: {
-        fetchIframeUrl: function () {
-            Nova.request().get('/nova-vendor/kpi-dashboard/')
-                .then(response => {
-                    this.iframeUrl = response.data.data.iframeUrl;
-                })
-        }
-    }
+  },
 }
 </script>
 
-<style>
-/* Scoped Styles */
+<style scoped>
+iframe {
+  width: 100%;
+  min-height: 100vh;
+}
 </style>
